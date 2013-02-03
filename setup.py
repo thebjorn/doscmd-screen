f91159b2e452dabbca799699f64e79cafed385c9
Bjorn Pettersen <bp@datakortet.no>
2013-02-03 20:41:20 +0100

    Update setup.py file with description and classifiers.

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""dosbox-screen: The Screen class allows you to do positioned writes to
+   the dos terminal (dosbox). It also allows you to specify the colors
+   for foreground and background, to the extent the dosbox allows.
+"""
+
+classifiers = """\
+Development Status :: 3 - Alpha
+Environment :: Win32 (MS Windows)
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Operating System :: Microsoft :: MS-DOS
+Programming Language :: Python
+Topic :: Software Development :: Libraries
+Topic :: System :: Shells
+Topic :: Terminals
+"""
+
 from distutils.core import setup
 
 
